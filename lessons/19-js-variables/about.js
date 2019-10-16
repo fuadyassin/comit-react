d8ab869734488e62acb20425a7db7f72eacd07d5
fuadyassin <fuaday@yahoo.com>
2019-10-15 20:28:52 -0600

    completed lesson 19

--- lessons/19-js-variables/about.js
+++ lessons/19-js-variables/about.js
@@ -11,8 +11,8 @@ age = 55;
 nextCity = city;
 
 
-conssole.log("Month of Birth: ", monthofBirth);
-conssole.log("First name: ", firstName);
+console.log("Month of Birth: ", monthofBirth);
+console.log("First name: ", firstName);
 console.log("Last Name: ", lastName);
 console.log("city: ", city);
 console.log("Age: ", age);
